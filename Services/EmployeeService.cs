69738bd1406d8098c69972c2e1571a38b3ab95d8
Saksham <93081133+fermi6-626@users.noreply.github.com>
2024-07-02 15:55:03 +0545

    19,11,61: getTotal, paginate, queryable fixed CreateEmployeeAsync()

--- Services/EmployeeService.cs
+++ Services/EmployeeService.cs
@@ -1,4 +1,5 @@
-﻿using api.Models;
+﻿using api.Filters;
+using api.DTO;
 using static Global;
 
 namespace EmployeesWebApp.Services
@@ -15,29 +16,37 @@ namespace EmployeesWebApp.Services
             _httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public async Task<IEnumerable<Employee>> GetEmployeesAsync()
+        public async Task<IEnumerable<EmployeesDto>> GetEmployeesAsync(string name, int pagenumber, int pagesize)
         {
-            HttpResponseMessage response = await _httpClient.GetAsync("api/employees");
+            var query = new EmployeeSearchQuery
+            {
+                Name = name,
+                PageNumber = pagenumber,
+                PageSize = pagesize
+            };
+
+            var queryString = $"?name={query.Name}&pagenumber={query.PageNumber}&pagesize={query.PageSize}";
+            HttpResponseMessage response = await _httpClient.GetAsync($"api/employees{queryString}");
             response.EnsureSuccessStatusCode();
-            var employees = await response.Content.ReadFromJsonAsync<IEnumerable<Employee>>();
+            IEnumerable<EmployeesDto>? employees = await response.Content.ReadFromJsonAsync<IEnumerable<EmployeesDto>>();
             return employees ?? [];
         }
 
-        public async Task<Employee?> GetEmployeeByIdAsync(int id)
+        public async Task<EmployeesDto?> GetEmployeeByIdAsync(int id)
         {
             HttpResponseMessage response = await _httpClient.GetAsync($"api/employees/{id}");
             response.EnsureSuccessStatusCode();
-            var employee = await response.Content.ReadFromJsonAsync<Employee>();
+            var employee = await response.Content.ReadFromJsonAsync<EmployeesDto>();
             return employee;
         }
 
-        public async Task<bool> CreateEmployeeAsync(Employee employee)
+        public async Task<bool> CreateEmployeeAsync(EmployeesDto employee)
         {
             HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/employees", employee);
             return response.IsSuccessStatusCode;
         }
 
-        public async Task<bool> UpdateEmployeeAsync(Employee employee)
+        public async Task<bool> UpdateEmployeeAsync(EmployeesDto employee)
         {
             HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"api/employees/{employee.Id}", employee);
             return response.IsSuccessStatusCode;
@@ -47,6 +56,14 @@ namespace EmployeesWebApp.Services
         {
             HttpResponseMessage response = await _httpClient.DeleteAsync($"api/employees/{id}");
             return response.IsSuccessStatusCode;
+        }
+
+        public async Task<int> GetTotal()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("api/employees/all");
+            response.EnsureSuccessStatusCode();
+            IEnumerable<EmployeesDto>? employees = await response.Content.ReadFromJsonAsync<IEnumerable<EmployeesDto>>();
+            return employees?.Count() ?? 00;
         }
     }
 }
